c448f1c66b001be0709febd386c70fa31507766d
Tigist Damesa <damesa@tigistpro.me>
2022-07-06 14:41:39 -0400

    Added client

---
+++ client/src/config.ts
@@ -0,0 +1,12 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'e9z6tzxf32'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`;
+// export const apiEndpoint = 'http://localhost:7412/dev';
+
+export const authConfig = {
+  // TODO: Create an Auth0 application and copy values from it into this map. For example:
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+  domain: 'dev-wh685a43.us.auth0.com',            // Auth0 domain
+  clientId: 'G83oRC4TeqUN6MjyWWo9K2kIrJjHWbfE',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
